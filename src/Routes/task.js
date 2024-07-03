5e3aaf0171a6f4e595876dfccffd69721e782d6f
DiegoMD4 <diegolink4@hotmail.com>
2024-07-03 16:51:53 -0600

    Endpoint: DELETE

--- src/Routes/task.js
+++ src/Routes/task.js
@@ -14,14 +14,13 @@ router.post("/post", (req, res) => {
     due_date: req.body.due_date
   }
 
- 
   let query = `INSERT INTO tasks (title, description, due_date) VALUES (?, ?, ?)`;
   connection.query(query, [requestBody.title, requestBody.description, requestBody.due_date], (err, result) => {
     if (err) {
       console.error('Error executing query:', err.stack);
     } else {
-      res.status(200).json({message: 'inserted correctly'});
-      return  console.log(result);
+      res.status(200).json({ message: 'inserted correctly' });
+      return console.log(result);
     }
   });
 });
@@ -29,8 +28,20 @@ router.post("/post", (req, res) => {
 router.put("/put", (req, res) => {
   res.send("This is put task view");
 });
-router.delete("/delete", (req, res) => {
-  res.send("This is delete task view");
+
+
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+
+  let sql = `DELETE FROM tasks WHERE id = ${id}`;
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.error('Error executing query:', err.stack);
+    } else {
+      res.status(200).json({ message: 'deleted correctly' });
+      return console.log(result);
+    }
+  });
 });
 
 export default router;
